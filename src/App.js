f19836adacfef30843cc2d506731123fcba3382c
Liron Hamra <lironhamra@Lirons-MacBook-Pro.local>
2022-06-06 05:44:04 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { useState } from "react";
-import './App.css';
-//Importing COmponents
+import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
-import FormC from "./components/FormC";
+
 function App() {
   const [inputText, setInputText] = useState("");
-  const [todos, setTodos] = useState ([]);
+  const [todos, setTodos] = useState([]);
+
+  const clearList = () => {
+    setTodos([]);
+  };
+
   return (
     <div className="app">
       <header>
         <h1>Todo App</h1>
       </header>
-      <Form 
-      inputText={inputText} 
-      todos={todos} 
-      setTodos={setTodos} 
-      setInputText={setInputText} />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
       <TodoList setTodos={setTodos} todos={todos} />
-      <FormC />
+      <div className="clearListContainer">
+        <button className="clearBtn" onClick={clearList}>
+          Clear List
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,22 @@
 import React from "react";
-import Form from "./Form";
-//Import Components
 import Todo from "./Todo";
 
 const TodoList = ({ todos, setTodos }) => {
-    return(
-        <div className="todo-container">
+  return (
+    <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo) => (
-        <Todo 
-        setTodos={setTodos} 
-        todos={todos} 
-        key={todo.id} 
-        todo={todo}
-        text={todo.text} 
-        /> 
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+          />
         ))}
       </ul>
     </div>
-    );
+  );
 };
 
 export default TodoList;
